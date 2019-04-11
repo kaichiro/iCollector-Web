e59c748b46532d95b8ffcc51b04710174d987cb9
kaichiro <kcfg23@gmail.com>
2019-04-11 08:37:04 -0300

    Refatorando projeto para formato inicial

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h3>App Component Work's!!!</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
